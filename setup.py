01d992bd40adfcd351944301b47755242307afdc
Simon de Haan <simon@soocial.com>
2010-11-19 14:32:08 +0200

    updating setup.py for publishing to pypi

--- setup.py
+++ setup.py
@@ -6,9 +6,23 @@ setup(
     url = 'http://github.com/smn/bellville',
     license = 'BSD',
     description = "Clickatell HTTP library",
+    long_description = open('README.md', 'r').read(),
     author = 'Simon de Haan',
+    author_email = "simon@praekeltfoundation.org",
     packages = find_packages('src'),
     package_dir = {'': 'src'},
-    install_requires = ['setuptools',],
+    install_requires = [
+        'setuptools',
+        'pyOpenSSL',
+        'nose',
+    ],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
 
