d9552bc870590d110526295d735b51d918c71d8c
Simon de Haan <simon@praekeltfoundation.org>
2011-11-21 11:57:13 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ def listify(filename):
 
 setup(
     name = "python-clickatell",
-    version = "0.1.2",
+    version = "0.1.3",
     url = 'http://github.com/smn/bellville',
     license = 'BSD',
     description = "Clickatell HTTP library",
